5ac59c09cfb0b5318b8b3762bb6726cea035cf06
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-04-09 16:44:12 +0200

    created ground for fetching movies

--- js/redux/actions/movies.js
+++ js/redux/actions/movies.js
@@ -1,4 +1,6 @@
 const START_SEARCHING = "START_SEARCHING"
+const SEARCH_MOVIES_ERROR = "SEARCH_MOVIES_ERROR"
+const SEARCH_MOVIES_OK = "SEARSEARCH_MOVIES_OKCHED_MOVIES"
 
 const startSearching =  () => {
     return {
@@ -6,6 +8,26 @@ const startSearching =  () => {
     }
 }
 
+const searchMoviesError = (error) => {
+    return {
+        type: SEARCH_ERROR,
+        error
+    }
+}
+
+const searchMoviesOk = (movies) => {
+    return {
+        type: SEARCH_ERROR,
+        movies
+    }
+}
+
+const fetchMovies = (movieName) => (dispatch) => {
+    dispatch(startSearching())
+
+    console.log("przeszło", movieName);
+}
+
 export {
-    START_SEARCHING, startSearching
+    START_SEARCHING, SEARCH_MOVIES_ERROR, SEARCH_MOVIES_OK, fetchMovies
 }
