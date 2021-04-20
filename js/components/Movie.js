641da74a22df6bb310fd08e5b8ec4e784e64b9d9
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-04-20 16:19:27 +0200

    Added feature: now searched movies are link to movie details

--- js/components/Movie.js
+++ js/components/Movie.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Stars from "../containers/Stars";
+import { Link } from "react-router-dom";
 
 const Movie = ({movie}) => {
     
     return (
         <div className="movie">
-            <h5>{movie.Title} ({movie.Year}) ID: {movie.imdbID}</h5>
-            <img className="poster" src={movie.Poster} alt={movie.Title}/>
+            <Link to={`/details/${movie.imdbID}`} className="link-text">
+                <h5>{movie.Title} ({movie.Year}) ID: {movie.imdbID}</h5>
+                <img className="poster" src={movie.Poster} alt={movie.Title}/>
+            </Link>
             <Stars movie={movie}/>
         </div>
     )
