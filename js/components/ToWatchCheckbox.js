79d3dca25ccbb77c5033607730c776328ac35fa7
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-05-11 16:45:45 +0200

    Added feture: checkbox is defaultly checked if movie is on list of movies to watch

--- js/components/ToWatchCheckbox.js
+++ js/components/ToWatchCheckbox.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ToWatchCheckbox = ({movie, moviesToWatch, toggleToWatchMovie}) => {
+    const [checkBoxValue, setCheckBoxValue] = useState()
+
     const handleChange = () => {
         toggleToWatchMovie(movie);
     }
 
+    useEffect(() => {
+        let isMovieOnList = moviesToWatch.findIndex((movieToWatch) => {
+            return movieToWatch.imdbID === movie.imdbID;
+        })
+        if (isMovieOnList !== -1) {
+            setCheckBoxValue(true);
+        } else {
+            setCheckBoxValue(false);
+        }
+    }, [])
+
     return (
         <label>Chcę obejrzeć: 
-        <input type="checkbox" id={movie.imdbID} onChange={handleChange}/>
+        <input type="checkbox" id={movie.imdbID} onClick={handleChange} defaultChecked={checkBoxValue}/>
         </label>
     )
 }
