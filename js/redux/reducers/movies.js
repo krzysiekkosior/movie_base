660a8d3525d13a638e9ea540608b4a8e8ad783bb
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-05-11 15:25:50 +0200

    removed unused function

--- js/redux/reducers/movies.js
+++ js/redux/reducers/movies.js
@@ -53,13 +53,4 @@ const movies = (state = initState, action) => {
     }
 }
 
-function changeRating( value, desc ) {
-    for (var i in projects) {
-      if (projects[i].value == value) {
-         projects[i].desc = desc;
-         break; //Stop this loop, we found it!
-      }
-    }
- }
-
 export default movies;
